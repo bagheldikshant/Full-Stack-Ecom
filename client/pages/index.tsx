fe4eb6b6dced85e01725f25f5dd3534638ed6136
Dikshant <94803450+bagheldikshant@users.noreply.github.com>
2023-02-11 13:00:02 +0530

    new commit

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p className='bg-[red]'>
-            Get started by editing&nbsp;
+            Get started b editing&nbsp;
             <code className={styles.code}>pages/index.tsx</code>
           </p>
           <div>
